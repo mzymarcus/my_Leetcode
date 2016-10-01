58f8f0bd447664205329aa0d54dc0f2428784f43
Marcus Ma <Marcus Ma>
2016-10-01 13:20:55 -0400

    154

--- 154.java
+++ 154.java
@@ -30,3 +30,32 @@ public class Solution {
         return min;
     }
 }
+
+//nice version
+public class Solution {
+    public int findMin(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+        int left = 0, right = nums.length-1, middle = (left+right)/2, min = nums[0];
+        while(left < right-1){
+            if(nums[left] < nums[middle]){
+                min = Math.min(nums[left], min);
+                left = middle;
+                middle = (left+right)/2;
+            }
+            else if(nums[left] > nums[middle]){
+                min = Math.min(nums[middle], min);
+                right = middle;
+                middle = (left+right)/2;
+            }
+            else {
+                left++;
+                middle = (left+right)/2;
+            }
+        }
+        min = Math.min(min, nums[left]);
+        min = Math.min(min, nums[right]);
+        return min;
+    }
+}
